ba9ea1652dc37441f5019e7676a7462ab14fbb73
Facundo Mondino <mondinofacundo254@gmail.com>
2021-05-23 18:45:23 -0300

    ITER 2 COMPROBACION FECHA

--- codigoFuente/Program.cs
+++ codigoFuente/Program.cs
@@ -15,12 +15,16 @@ namespace TP4_Diseño
             CargarListas.actividadesAutorizadas.Add(actividadTres);
 
             //Creacion de objetos persona. Agrega a la lista todas las personas.
-            Persona personaUno = new Persona("Pepito Martinez", "67890098", "domicilioUno", "telefonoUno", "emailUno", actividadUno);
+            Persona personaUno = new Persona("Pepito Martinez", "67890098", "domicilioUno", "telefonoUno", "emailUno", actividadUno, DateTime.Parse("19/05/2021"));
             CargarListas.personas.Add(personaUno);
-            Persona personaDos = new Persona("Fulanito DeTal", "98765432", "domicilioDos", "telefonoDos", "emailDos", actividadDos);
+            Persona personaDos = new Persona("Fulanito DeTal", "98765432", "domicilioDos", "telefonoDos", "emailDos", actividadDos, DateTime.Parse("24/05/2021"));
             CargarListas.personas.Add(personaDos);
-            Persona personaTres = new Persona("Susana Lopez", "96765430", "domicilioTres", "telefonoTres", "emailTres", actividadTres);
+            Persona personaTres = new Persona("Susana Lopez", "96765430", "domicilioTres", "telefonoTres", "emailTres", actividadTres,DateTime.Parse("25/05/2021"));
             CargarListas.personas.Add(personaTres);
+
+            //Creacion de objetos empresa.
+            Empresa empresaUno = new Empresa("EmpresaUno", "100", "DomicilioUno", "LocalidadUno", "emailUno", "telefonoUno", actividadUno);
+            Empresa empresaDos = new Empresa("EmpresaDos", "200", "DomicilioDos", "LocalidadDos", "emailDos", "telefonoDos", actividadTres);
 
             //Muestra la lista con todas las personas que quieren acceder al area. (no se verifico aun).
             Console.WriteLine("\nLISTA DE PERSONAS:\n");
@@ -48,9 +52,10 @@ namespace TP4_Diseño
             public string telefono { get; set; }
             public string email { get; set; }
             public Actividad nombreActividad { get; set; }
+            public DateTime fecha { get; set; }
 
             //Metodo constructor de la clase Persona.
-            public Persona(string nombreApellido, string dni, string domicilio, string telefono, string email, Actividad nombreActividad)
+            public Persona(string nombreApellido, string dni, string domicilio, string telefono, string email, Actividad nombreActividad, DateTime fecha)
             {
                 this.nombreApellido = nombreApellido;
                 this.dni = dni;
@@ -58,6 +63,7 @@ namespace TP4_Diseño
                 this.telefono = telefono;
                 this.email = email;
                 this.nombreActividad = nombreActividad;
+                this.fecha = fecha;
             }
         }
         public class Actividad//Clase Actividad.
@@ -78,11 +84,33 @@ namespace TP4_Diseño
 
             public static List<Persona> personasAutorizadas = new List<Persona>();
         }
+        public class Empresa
+        {
+            public string razonSocial { get; set;}
+            public string cuit { get; set; }
+            public string domicilio { get; set; }
+            public string localidad { get; set; }
+            public string email { get; set; }
+            public string telefono { get; set; }
+            public Actividad actividadEmpresa { get; set;}
+
+            public Empresa(string razonSocial, string cuit, string domicilio, string localidad, string email, string telefono, Actividad actividadEmpresa)
+            {
+                this.razonSocial = razonSocial;
+                this.cuit = cuit;
+                this.domicilio = domicilio;
+                this.localidad = localidad;
+                this.email = email;
+                this.telefono = telefono;
+                this.actividadEmpresa = actividadEmpresa;
+            }
+        }
+ 
         public static void mostrarListaPersonas()//Metodo, muestra lista con personas.
         {
             foreach (var a in CargarListas.personas)
             {
-                Console.WriteLine($"{a.nombreApellido} - {a.dni} - {a.domicilio} - {a.telefono} - {a.email} - {a.nombreActividad.nombreActividad}");
+                Console.WriteLine($"{a.nombreApellido} - {a.dni} - {a.domicilio} - {a.telefono} - {a.email} - {a.nombreActividad.nombreActividad} - {a.fecha}");
             }
         }
         public static void mostrarListaAcividadesAutorizadas()//Metodo, muestra lista actividades autorizadas.
@@ -126,6 +154,7 @@ namespace TP4_Diseño
         {
             string numDni;
             int ban = 0;
+            int banDos = 0;
 
             Console.WriteLine("\nIngrese el DNI de la persona para verificar si existe en la lista:\n");
             numDni = Console.ReadLine();
@@ -135,14 +164,26 @@ namespace TP4_Diseño
                 if (d.dni == numDni)
                 {
                     ban = 1;
-                    Console.WriteLine($"{d.nombreApellido}");
+
+                    if (DateTime.Now <= d.fecha)
+                    {
+                        banDos = 1;
+                        Console.WriteLine(d.fecha);
+                    }
                 }
             }
-            if (ban == 1)
+            if (ban == 1 && banDos == 1)
             {
-                Console.WriteLine("\n---LA PERSONA EXISTE EN LA LISTA, REALIZA UNA ACTIVIDAD AUTORIZADA POR ENDE PUEDE CIRCULAR POR EL AREA---\n");
+                Console.WriteLine("\n---LA PERSONA EXISTE EN LA LISTA, REALIZA UNA ACTIVIDAD AUTORIZADA, SU FECHA ES VIGENTE, POR ENDE PUEDE CIRCULAR POR EL AREA---\n");
             }
-            else Console.WriteLine("\n---LA PERSONA NO EXISTE EN LA LISTA, NO REALIZA UNA ACTIVIDAD AUTORIZADA POR ENDE NO PUEDE CIRCULAR POR EL AREA---\n");
+            if (ban == 1 && banDos != 1)
+            {
+                Console.WriteLine("\n---LA PERSONA EXISTE EN LA LISTA, REALIZA UNA ACTIVIDAD AUTORIZADA, PERO SU FECHA ESTA VENCIDA, POR ENDE NO PUEDE CIRCULAR POR EL AREA---\n");
+            }
+            if(ban !=1)
+            {
+                Console.WriteLine("\n--LA PERSONA NO EXISTE EN LA LISTA---\n");
+            }
         }
     }
 }
